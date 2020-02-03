a30565dc1b8af438b9e9c1082335f1352d03f527
aidan1221 <aidandwolfe@gmail.com>
2020-02-02 16:50:46 -0800

    run_parent file for operating parent scraper

---
+++ webscraping/billscraper.py
@@ -0,0 +1,21 @@
+from webscraper import Webscraper
+
+
+class Billscraper(Webscraper):
+
+    URL_116 = "https://www.congress.gov/search?q=%7B%22source%22%3A%22legislation%22%2C%22congress%22%3A%22116" \
+              "%22%2C%22chamber%22%3A%22House%22%2C%22type%22%3A%22bills%22%7D"
+    URL_115 = "https://www.congress.gov/search?q={%22source%22:%22legislation%22,%22chamber%22:%22House%22,%22" \
+              "type%22:%22bills%22,%22congress%22:115}&searchResultViewType=expanded&KWICView=false"
+
+    def __init__(self, chamber, congressionalClass):
+        super(Billscraper, self).__init__()
+        self.chamber = chamber
+        self.congressionalClass = congressionalClass
+
+    def getHouseLegislationPage(self):
+
+        if self.congressionalClass == 116:
+            url = self.URL_116
+
+        self.open(url)
--- webscraping/webscraper.py
+++ webscraping/webscraper.py
@@ -15,9 +15,21 @@ class Webscraper:
     CSS_SELECTOR = By.CSS_SELECTOR
 
     # Utility methods
-    def wait_for_element_visible_by_css(self, css_selector, duration=10):
+    def wait_for_element_invisible_by_css(self, css_selector, duration=10):
+        """
+        Waits for element invisibility by css selector
+        :param css_selector: the css selector selenium for use for the wait condition
+        :param duration: the duration of time the driver will wait before throwing an exception
+        :return: void
+        """
 
         load_condition = EC.invisibility_of_element((By.CSS_SELECTOR, css_selector))
+
+        WebDriverWait(self.DRIVER, duration).until(load_condition)
+
+    def wait_for_element_visible_by_css(self, css_selector, duration=10):
+
+        load_condition = EC.visibility_of_element((By.CSS_SELECTOR, css_selector))
 
         WebDriverWait(self.DRIVER, duration).until(load_condition)
 
