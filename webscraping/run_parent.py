1eafe78069b2008c27e3d175942167822a7b0758
Julie Rutherford-Fields <julie@Julies-MacBook-Air.local>
2020-02-08 14:26:43 -0800

    address merge conflict

--- webscraping/run_parent.py
+++ webscraping/run_parent.py
@@ -1,3 +1,5 @@
+from datetime import time
+
 from webscraper import Webscraper
 from billscraper import Billscraper
 
@@ -8,3 +10,14 @@ bs.scrape_bills()
 
 
 bs.close()
+
+
+# ws.open("https://www.congress.gov")
+#
+# element = ws.DRIVER.find_element_by_css_selector("#search")
+#
+# element.send_keys("alabama")
+# element.submit()
+
+
+time.sleep(10)
